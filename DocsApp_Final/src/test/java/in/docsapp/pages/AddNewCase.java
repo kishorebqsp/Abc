8538542a6f1250b5f2e34c09950487cb77e180da
kishorebqsp <35919450+kishorebqsp@users.noreply.github.com>
2018-02-27 18:35:25 +0530

    uncomment
    uncomment

--- DocsApp_Final/src/test/java/in/docsapp/pages/AddNewCase.java
+++ DocsApp_Final/src/test/java/in/docsapp/pages/AddNewCase.java
@@ -43,7 +43,7 @@ public class AddNewCase {
 	
 	public WebElement getAddNewCasePhoneNumberTextBox()
 	{
-		//return eleAddNewCasePhoneNumberTextBox;
+		return eleAddNewCasePhoneNumberTextBox;
 	}
 	@FindAll
 	({
